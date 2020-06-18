8ed4455aefbe65faf94413cbc9d7d6453fbfcf64
MrLotU <j.koopman@jarict.nl>
2020-06-18 17:37:31 +0200

    Licensing

--- Sources/PublishCLICore/NIOHTTPServer.swift
+++ Sources/PublishCLICore/NIOHTTPServer.swift
@@ -1,3 +1,9 @@
+/**
+*  Publish
+*  Copyright (c) John Sundell 2019
+*  MIT license, see LICENSE file for details
+*/
+
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftNIO open source project
